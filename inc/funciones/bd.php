30d9131ea838b1119170caabf4882d7918e7c1f7
Francisco Lopez <franlopbri@gmail.com>
2020-07-14 14:14:18 -0500

    queda insercion de datos

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -7,7 +7,7 @@ define('DB_NAME', 'agendaphp');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME);
 
-echo $conn->ping();
+//echo $conn->ping();
 
 
 
--- inc/modelos/modelo-contactos.php
+++ inc/modelos/modelo-contactos.php
@@ -1,5 +1,38 @@
 <?php 
 
-    echo json_encode($_POST);
+    if($_POST['accion'] == 'crear'){
+        require_once('../funciones/bd.php');
+
+        //validar las entradas
+
+        $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+        $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+        $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+
+        try {
+            $statement = $conn->prepare("INSERT INTO  contactos (nombre, empresa, telefono) VALUES (?, ?, ?)");
+            $statement->bind_param("sss", $nombre, $empresa, $telefono);
+            $statement->execute();
+            if($statement->affected_rows == 1){
+                $respuesta = array(
+                    'respuesta' => 'correcto',
+                    'datos' => array(
+                        'nombre' => $nombre,
+                        'empresa' => $empresa,
+                        'telefono' => $telefono,
+                        'id_insertado' => $statement->insert_id
+                    ) 
+                );
+            }
+            $statement->close();
+            $conn->close();
+        } catch (Exception $e) {
+            $respuesta = array(
+                'error' => $e->getMessage()
+            );
+        }
+    }
+
+    echo json_encode($respuesta);
 
 ?>
