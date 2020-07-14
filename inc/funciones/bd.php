36c1eefc879d8b9279b65e1d015c76cac9130097
Francisco Lopez <franlopbri@gmail.com>
2020-07-14 13:51:48 -0500

    configurando base de datos

---
+++ inc/funciones/bd.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_USUARIO', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'agendaphp');
+
+$conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME);
+
+echo $conn->ping();
+
+
+
+?>
