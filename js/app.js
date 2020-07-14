44847c1e3629da3e3a5eeda5f6cbaa10ca1b24c5
Francisco Lopez <franlopbri@gmail.com>
2020-07-14 12:53:22 -0500

    crea archivo php y conexion con ajax

--- js/app.js
+++ js/app.js
@@ -11,13 +11,47 @@ function leerFormulario(e){
     
     const nombre = document.querySelector('#nombre').value,
           empresa = document.querySelector('#empresa').value,
-          telefono = document.querySelector('#telefono').value
+          telefono = document.querySelector('#telefono').value,
+          accion = document.querySelector('#accion').value;
 
     if(nombre === '' || empresa === '' || telefono === ''){
         mostrarNotificacion('Todos los Campos son Obligarotios', 'error');
     } else {
-        console.log('Tiene algo');
+        //pasa la validacion
+        const infoContacto = new FormData();
+        infoContacto.append('nombre', nombre);
+        infoContacto.append('empresa', empresa);
+        infoContacto.append('telefono', telefono);
+        infoContacto.append('accion', accion);
+
+        //console.log(...infoContacto);
+
+        if(accion === 'crear'){
+            insertarBD(infoContacto);
+        } else {
+
+        }
     }
+}
+
+function insertarBD(data){
+    //llamado a ajax
+
+    //Crear el objeto
+    const xhr = new XMLHttpRequest();
+
+    //abrir la conexion
+    xhr.open('POST', 'inc/modelos/modelo-contactos.php', true);
+
+    //pasar los datos
+    xhr.onload = function(){
+        if(this.status === 200){
+            console.log(JSON.parse(xhr.responseText));
+        }
+    }
+
+    //enviar los datos
+    xhr.send(data);
 }
 
 function mostrarNotificacion(mensaje, clase){
