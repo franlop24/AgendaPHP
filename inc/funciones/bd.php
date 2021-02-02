3607867b9b133a774fb574cc6e9c989389f09853
Francisco Lopez <franlopbri@gmail.com>
2021-02-01 19:45:53 -0600

    agregando base de datos

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_USUARIO', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost:3306');
+define('DB_PASSWORD', 'tutorial.2020');
+define('DB_HOST', 'localhost:23306');
 define('DB_NAME', 'agendaphp');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME);
