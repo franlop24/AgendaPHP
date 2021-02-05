4fec944c93bb4695d6c46d2761815df85e2a375d
Francisco Lopez <franlopbri@gmail.com>
2021-02-04 23:36:46 -0600

    generando contenedor php y mysql docker

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -1,14 +1,12 @@
 <?php
 
 define('DB_USUARIO', 'root');
-define('DB_PASSWORD', 'tutorial.2020');
-define('DB_HOST', 'localhost:23306');
+define('DB_PASSWORD', 'example');
+define('DB_HOST', 'db');
 define('DB_NAME', 'agendaphp');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME);
 
 //echo $conn->ping();
-
-
 
 ?>
